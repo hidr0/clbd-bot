428c691ec5c45aedd200c32e0a3e49855f72b08f
Mihail Kirilov <hidr0.frbg@gmail.com>
2024-04-18 23:19:12 +0300

    Finishing up

--- deploy-commands.js
+++ deploy-commands.js
@@ -11,19 +11,33 @@ const commands = [
     }
 ];
 
+const commandId = 1227216829387378749;
+// 1217399916704563240;
 const rest = new REST({ version: '10' }).setToken(token);
 
-(async () => {
-    try {
-        console.log('Started refreshing application (/) commands.');
+console.log('Started refreshing application (/) commands.');
 
-        await rest.put(
-            Routes.applicationGuildCommands(clientId, guildId),
-            { body: commands },
-        );
+// rest.put(
+//     Routes.applicationGuildCommands(clientId, guildId),
+//     { body: commands },
+// ).then((result) =>{
+//     console.log(result);
+//     console.log('Successfully reloaded application (/) commands.');
+// }).catch((error) => {
+//     console.error(error);
+// });
 
-        console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-        console.error(error);
-    }
-})();
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+	.then(() => console.log('Successfully deleted all guild commands.'))
+	.catch(console.error);
+
+// // for guild-based commands
+// rest.delete(Routes.applicationGuildCommand(clientId, guildId, commandId))
+// 	.then(() => console.log('Successfully deleted guild command'))
+// 	.catch(console.error);
+
+// // // for global commands
+// rest.delete(Routes.applicationCommand(clientId, commandId))
+// 	.then(() => console.log('Successfully deleted application command'))
+// 	.catch(console.error);
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const { Client, GatewayIntentBits, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
-const { token, newsChannelId } = require('./secrets.json'); // Ensure your config contains the bot token
+const { token, newsChannelId, mysqlConfig } = require('./secrets.json'); // Ensure your config contains the bot token
 const { publishNewsCommand } = require('./config.json'); // Ensure your config contains the bot token
+
+const mysql = require('mysql');
+const connection = mysql.createConnection(mysqlConfig);
+connection.connect();
 
 const client = new Client({
     intents: [
@@ -24,57 +28,91 @@ const FIELDS = [
     { id: 'title', label: "Какво е заглавието на новината?", style: TextInputStyle.Short, required: true },
     { id: 'link', label: "Какъв е линка на новината?", style: TextInputStyle.Short, required: true },
     { id: 'description', label: "Какво е описанието на новината", style: TextInputStyle.Paragraph, required: false },
-].map( (f) => {
+].map((f) => {
     return new ActionRowBuilder().addComponents(
         new TextInputBuilder()
             .setCustomId(f.id)
             .setLabel(f.label)
             .setStyle(f.style)
             .setRequired(f.required)
-            .setValue("Test123")
     )
 })
-
-
 
 
 // Init modal based on command name
 client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
-    if (interaction.commandName === publishNewsCommand) {
-        const modal = new ModalBuilder().setCustomId(MODAL.id).setTitle(MODAL.title);
-        modal.addComponents(FIELDS);
+    if (interaction.commandName !== publishNewsCommand) return;
+    const modal = new ModalBuilder().setCustomId(MODAL.id).setTitle(MODAL.title);
+    modal.addComponents(FIELDS);
 
-        // Show the modal to the user
-        await interaction.showModal(modal);
-    }
+    interaction.showModal(modal).then(response => {
+        ////
+    }).catch(error => {
+        logError(interaction, error)
+    });
 });
 
 // Handle modal submission
 client.on('interactionCreate', async interaction => {
     if (!interaction.isModalSubmit()) return;
-    if (interaction.customId === MODAL.id) {
-        console.log(interaction)
+    if (interaction.customId !== MODAL.id) return;
 
-        arr = [
-            `Заглавие: ${interaction.fields.getTextInputValue('title')}`,
-            `Описание: ${interaction.fields.getTextInputValue('description')}`,
-            `Link: ${interaction.fields.getTextInputValue('link')}`,
-            `Публикувана от: ${interaction.user.username} / ${interaction.user.globalName}`
-        ]
+    const parsed = parseInteraction(interaction);
 
-        messageToSend = arr.join("\n")
+    let sentMessage = await interaction.reply({ content: `Вашата новина е побликувана`, fetchReply: true, ephemeral: true });
 
-        await interaction.reply(`Received your request`);
+    const targetChannel = await client.channels.fetch(newsChannelId);
+    if (targetChannel && targetChannel.isTextBased()) {
+        targetChannel.send(getMessage(parsed)).then( async (response) => {
+            sentMessage = await interaction.editReply({ content: `${sentMessage.content}\n Вашата новина е побликувана в канала за новини` });
 
-        const targetChannel = await client.channels.fetch(newsChannelId);
-        if (targetChannel && targetChannel.isTextBased()) {
-            targetChannel.send(messageToSend).then(result => {
-                console.log(result)
-            });
-        } else {
-            console.log('Channel not found or is not a text-based channel.');
-        }
+            insert(parsed, response.id);
+
+            sentMessage = await interaction.editReply({ content: `${sentMessage.content}\n Вашата новина е запазена в базата` });
+        }).catch((error) => {
+            logError(interaction, error)
+        });
+    } else {
+        logError(interaction, 'Channel not found or is not a text-based channel.')
     }
+
 });
+
+
+const insert = (parsed, messageId) => {
+    const insertStatement = `INSERT INTO news_submissions(user, message_id, title, link, body)
+    VALUES("${parsed.username}", "${messageId}", "${parsed.title}", "${parsed.link}", "${parsed.description}");`
+    connection.query(insertStatement, function (error, results, fields) {
+        if (error) {
+            console.log(error);
+        }
+    });
+}
+
+const parseInteraction = (interaction) => {
+    return {
+        title: interaction.fields.getTextInputValue('title'),
+        description: interaction.fields.getTextInputValue('description'),
+        link: interaction.fields.getTextInputValue('link'),
+        username: interaction.user.username,
+        globalName: interaction.user.globalName
+    }
+}
+
+const getMessage = (parsed) => {
+    arr = [
+        `Заглавие: ${parsed.title}`,
+        `Описание: ${parsed.description}`,
+        `Link: ${parsed.link}`,
+        `Публикувана от: ${parsed.username} / ${parsed.globalName}`
+    ];
+
+    return arr.join("\n");
+}
+
+const logError = (interaction, error) => {
+    interaction.followUp({content: `Случи се проблем, пиши на Мишо ${error}`, ephemeral: true});
+    console.log(error)
+}
